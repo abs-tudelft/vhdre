e73b3a4c69ae2e8f7956de7733b0e618d3bb7e89
Jeroen van Straten <jeroen.van.straten@gmail.com>
2021-01-11 21:03:34 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name = "vhdre",
-    version = "0.2",
+    version = "0.3",
     author = "Jeroen van Straten",
     author_email = "j.vanstraten-1@tudelft.nl",
     description = (
